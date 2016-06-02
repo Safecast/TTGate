31a8aa587c25b4540a017366adb735ef21b4116a
Ray Ozzie <ray@ozzie.net>
2016-06-02 14:13:54 -0400

    test

--- src/ttgate/ttgate.go
+++ src/ttgate/ttgate.go
@@ -8,15 +8,36 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
+    "log"
+    "github.com/tarm/serial"
 )
 
 func main() {
 
-	for i := 0; ; i++ {
-		fmt.Printf("Hello from golang..! %d\n", i)
-		time.Sleep(1 * time.Second)
-	}
+    c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 57600}
+    s, err := serial.OpenPort(c)
+    if (err != nil) {
+        log.Fatal(err);
+    }
+
+    for i := 0; ; i++ {
+        fmt.Printf("sys get ver:")
+
+        n, err := s.Write([]byte("sys get ver\r\n"))
+        if (err != nil) {
+            fmt.Printf("write err: %d");
+        } else {
+            buf := make([]byte, 128)
+            n, err = s.Read(buf)
+            if (err != nil) {
+                fmt.Printf("read err: %d");
+            } else {
+                log.Printf("%q", buf[:n])
+            }
+        }
+        time.Sleep(1 * time.Second)
+    }
 
 }
