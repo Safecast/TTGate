7eb68ad9b84a5847242fe68768cddc1f8a887e46
Ray Ozzie <ray@ozzie.net>
2016-06-13 23:04:29 -0700

    more bug fixing

--- src/ttgate/io.go
+++ src/ttgate/io.go
@@ -15,7 +15,7 @@ import (
     "io"
     "net"
     "github.com/tarm/serial"
-	"github.com/stianeikeland/go-rpio"
+    "github.com/stianeikeland/go-rpio"
 )
 
 var serialPort *serial.Port
@@ -32,7 +32,7 @@ func ioInit() bool {
 
     speed := 57600
 
-    s, err := serial.OpenPort(&serial.Config{Name: port, Baud: speed, ReadTimeout: (time.Second * 15)})
+    s, err := serial.OpenPort(&serial.Config{Name: port, Baud: speed, ReadTimeout: (time.Second * 5)})
     if (err != nil) {
         fmt.Printf("Cannot open %s\n", port)
         return false
@@ -54,142 +54,149 @@ func ioInit() bool {
 
 func ioInitMicrochip() {
 
-	err := rpio.Open()
-	if (err != nil) {
-		fmt.Printf("ioInitMicrochip: err %v\n", err)
-		return;
-	}
+    err := rpio.Open()
+    if (err != nil) {
+        fmt.Printf("ioInitMicrochip: err %v\n", err)
+        return;
+    }
 
-	fmt.Printf("ioInitMicrochip: Hardware reset...\n")
+    fmt.Printf("ioInitMicrochip: Hardware reset...\n")
 
-	// Note that this requires two things to be true:
-	// 1) On the back side of the RN2483/RN2903, use solder to close the gap of SJ1, which brings /RESET to Xbee Pin 17
-	// 2) Wire Xbee Pin 17 to the RPi's header Pin 36, which is BCM Pin 16 (http://pinout.xyz/pinout/pin36_gpio16)
-	pin := rpio.Pin(16)
+    // Note that this requires two things to be true:
+    // 1) On the back side of the RN2483/RN2903, use solder to close the gap of SJ1, which brings /RESET to Xbee Pin 17
+    // 2) Wire Xbee Pin 17 to the RPi's header Pin 36, which is BCM Pin 16 (http://pinout.xyz/pinout/pin36_gpio16)
+    pin := rpio.Pin(16)
 
-	pin.Output()       // Output mode
+    pin.Output()       // Output mode
 
-	pin.Toggle()       // Toggle pin (Low -> High -> Low)
-	time.Sleep(time.Second * 1)
-	pin.Toggle()       // Toggle pin (Low -> High -> Low)
-	time.Sleep(time.Second * 5)
+    pin.Toggle()       // Toggle pin (Low -> High -> Low)
+    time.Sleep(time.Second * 1)
+    pin.Toggle()       // Toggle pin (Low -> High -> Low)
+    time.Sleep(time.Second * 5)
 
-	rpio.Close()
+    rpio.Close()
 
-	fmt.Printf("ioInitMicrochip: ...completed\n");
-	
+    fmt.Printf("ioInitMicrochip: ...completed\n");
+
 }
 
 func ioRequestReinit() {
-	reinitRequested = true;
+    reinitRequested = true;
 }
 
 func InboundMain() {
 
-	// Do all initialization and reinitialization in this goroutine
+    // Do all initialization and reinitialization in this goroutine
     cmdInit()
 
-	// Loop reading from input, and dispatching to process it if something is received
+    // Loop reading from input, and dispatching to process it if something is received
     var thisbuf = make([]byte, 128)
     var prevbuf []byte = []byte("")
 
     for {
-		fmt.Printf("<rd>\n")
+
+        fmt.Printf("<rd>\n")
         n, err := serialPort.Read(thisbuf)
-		fmt.Printf("</rd>\n")
-        if (err != nil) {
+        fmt.Printf("</rd>\n")
 
-			// If EOF, it's because of a timeout with no input
-			if (err == io.EOF) {
-				if reinitRequested {
-					reinitRequested = false;
-					cmdReinit()
-				}
-			} else {
-                fmt.Printf("serial: read error %v\n", err)
+        if (err == io.EOF) {
+            err = nil
+            n = 0
+        }
+
+        if (n == 0) {
+            if reinitRequested {
+                reinitRequested = false;
+                cmdReinit()
+            } else {
+                time.Sleep(100 * time.Millisecond)
             }
+        }
 
-        } else {
+        if (err != nil) {
+            n = 0
+            fmt.Printf("serial: read error %v\n", err)
+        }
 
-			// Process the inbound data
+        if (n != 0) {
             prevbuf = ProcessInbound(bytes.Join([][]byte{prevbuf, thisbuf[:n]}, []byte("")))
-
         }
     }
+}
 
 }
 
 func ProcessInbound(buf []byte) []byte  {
 
-    length := len(buf)
-    begin := 0
-    end := 0
+length := len(buf)
+begin := 0
+end := 0
 
-    // Loop over the buffer, which could have multiple lines in it
+// Loop over the buffer, which could have multiple lines in it
 
-    for begin<length {
+for begin<length {
 
-        // Parse out a single line delineated by begin:end
+    // Parse out a single line delineated by begin:end
 
-        for end = begin; end<length; end++ {
+    for end = begin; end<length; end++ {
 
-            // Process the line if it ends in \r\n or \r or \n
+        // Process the line if it ends in \r\n or \r or \n
 
-            if (buf[end] == '\r' || buf[end] == '\n') {
+        if (buf[end] == '\r' || buf[end] == '\n') {
 
-                // Process if non-blank (which it will be on the \n of \r\n)
+            // Process if non-blank (which it will be on the \n of \r\n)
 
-                if (end > begin) {
-                    cmdProcess(buf[begin:end])
-                }
-
-                // Skip past this delimeter and look for the next command
-
-                begin = end+1
-                break
-
+            if (end > begin) {
+                cmdProcess(buf[begin:end])
             }
-        }
 
-        if (end >= length) {
+            // Skip past this delimeter and look for the next command
+
+            begin = end+1
             break
-        }
 
+        }
     }
 
-    // Return unprocessed portion of the buffer for next time
+    if (end >= length) {
+        break
+    }
 
-    return(buf[begin:])
+}
+
+// Return unprocessed portion of the buffer for next time
+
+return(buf[begin:])
 
 }
 
 func ioSendCommandString(cmd string) {
-    ioSendCommand([]byte(cmd))
+ioSendCommand([]byte(cmd))
 }
 
 func ioSendCommand(cmd []byte) {
 
-    fmt.Printf("ioSendCommand(%s)\n", cmd)
+fmt.Printf("ioSendCommand(%s)\n", cmd)
 
-    _, err := serialPort.Write(bytes.Join([][]byte{cmd, []byte("")}, []byte("\r\n")))
-    if (err != nil) {
-        fmt.Printf("write err: %d", err)
-    }
+_, err := serialPort.Write(bytes.Join([][]byte{cmd, []byte("")}, []byte("\r\n")))
+if (err != nil) {
+    fmt.Printf("write err: %d", err)
+}
 
-    fmt.Printf("ioSendCommand(%s) (sent)\n", cmd)
+fmt.Printf("ioSendCommand(%s) (sent)\n", cmd)
 
 }
 
 func getDeviceID() string {
-    ifs, _ := net.Interfaces()
-    for _, v := range ifs {
-        h := v.HardwareAddr.String()
-        if len(h) == 0 {
-            continue
-        }
-        return(h)
+ifs, _ := net.Interfaces()
+for _, v := range ifs {
+    h := v.HardwareAddr.String()
+    if len(h) == 0 {
+        continue
     }
-    return("");
+    return(h)
+}
+return("");
 }
 
 // eof
