7f0d6aae74e8ba44c51a896aa141acee4bb678d6
Ray Ozzie <ray@ozzie.net>
2016-11-18 10:49:55 -0800

    m

--- main.go
+++ main.go
@@ -4,6 +4,4 @@ type Telecast struct {}
 func (m *Telecast) Reset()                    { *m = Telecast{} }
 func (*Telecast) ProtoMessage()               {}
 func (m *Telecast) String() string            { return proto.CompactTextString(m) }
-//func (*Telecast) Descriptor() ([]byte, []int) { return []byte{}, []int{0} }
 func main() {}
-func test(msg *Telecast) {}
