077c50bc3de9df5bff59879fdd9fa3d3fc8fe032
Ray Ozzie <ray@ozzie.net>
2016-11-18 10:19:30 -0800

    m

--- main.go
+++ main.go
@@ -2,8 +2,6 @@
 package main
 
 import (
-    "fmt"
-    "github.com/golang/protobuf/proto"
     "github.com/rayozzie/teletype-proto/golang"
 )
 
@@ -11,20 +9,6 @@ import (
 func main() {
 }
 
-func cmdProcessReceivedTelecastMessage(msg *teletype.Telecast, pb []byte, snr float32) {
-
-    switch msg.GetDeviceType() {
-
-    case teletype.Telecast_TTGATE:
-        if msg.Message == nil {
-            msg.Message = proto.String("ping")
-            data, err := proto.Marshal(msg)
-            if err != nil {
-                fmt.Printf("marshaling error: ", err, data)
-            }
-        }
-
-    }
-
+func test(msg *teletype.Telecast) {
 
 }
