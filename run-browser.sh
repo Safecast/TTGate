5f6fc3351869ed151bd19a07b09f368f3210f3bd
Ray Ozzie <ray@ozzie.net>
2017-01-23 13:53:52 -0500

    m

--- run-browser.sh
+++ run-browser.sh
@@ -1,2 +1,5 @@
-# run midori under x11
+# Sleep to avoid any startup race conditions that manifested themselves
+# because the gateway hadn't yet started its web server
+sleep 60s
+# Run midori under x11
 startx $GOPATH/src/ttgate/run-midori.sh
